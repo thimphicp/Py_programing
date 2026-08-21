88b443ea1c96559b0c842e6383915a1801c5dfa5
include <thimphicp@gmail.com>
2026-08-21 11:50:29 +0900

    datatype

---
+++ 01_datatype/ex1.py
@@ -0,0 +1,33 @@
+# 변수
+a = 2
+b = 3
+print(a,b)
+
+# a = 2, b = 3
+# a = (2, b) = 3
+
+a = 2; b = 3
+a, b = 2, 3
+print(a, b)
+
+# 값 swap
+temp = a
+a = b
+b = temp
+print(a, b)
+
+a, b = b, a
+print(a, b)
+
+# name! = "뽀로로"
+# 2name = "크롱"
+# _age = 23
+# class = "클래스"
+
+이름 = "뽀로로"
+print(이름)
+
+student_name = "크롱?" # snake
+studentName = "크롱?" # camel
+
+MAX_SCORE = 200
