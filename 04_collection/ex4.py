a6e8f6e0355acc15ccb36a4bab7ccd8640a73d79
include <thimphicp@gmail.com>
2026-09-11 11:22:38 +0900

    ex4.py

--- 04_collection/ex4.py
+++ 04_collection/ex4.py
@@ -114,6 +114,7 @@ print(f"{days[hours.index(max(hours))]}")                                       
 
 # 4️⃣ 가장 높은 점수와 가장 낮은 점수 출력하기
 scores = (90, 85, 78, 92, 88, 76)
+result = sorted(scores)
 print(f"max 점수: {max(scores)}점, min 점수: {min(scores)}점")
 
                                                     # ✅ max 점수: 92점, min 점수: 76점
@@ -127,5 +128,14 @@ stocks = (
 )
 
 # 총 재고 금액 출력
-print(f"{sum(price * quantity for _, price, quantity in stocks)}원")
-                                                    # ✅ 총액: 21,000원
+total = sum(price * num for name, price, num in stocks)
+print(f"{total:,}원")
+
+stocks = (
+    ("사과", "바나나", "체리"),
+    (1000, 2000, 5000),
+    (5, 3, 2),
+)
+total = sum(price * num for _, price, num in zip(*stocks))
+print(f"{total:,}원")
+                                       # ✅ 총액: 21,000원
